42b059f4a2120c616d27bce08de66eefa476a63e
rlarlgsnzx <rlarlgnszx@naver.com>
2021-08-30 19:58:34 +0900

    kihoon's firstweb

---
+++ gitweb/index.js
@@ -0,0 +1,14 @@
+// const titlebtn = document.querySelector("#header .avatar");
+// const github = document.querySelector('.modal-overlay');
+// show_name = function(title,next){
+//     title.addEventListener("click", e=>{
+//         next.style.display="flex";
+//     })
+// }
+// show_name(titlebtn,github);
+// // show_name2 = function(title){
+// //     title.addEventListener("click", e=>{
+// //         console.log(title);
+// //     })
+// // }
+// // show_name2(titlebtn);
